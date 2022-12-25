d83bf39f717ded9d94f9131f258b06e94c823dc7
RizkiAshari <rizkiashari1026@gmail.com>
2022-12-26 00:02:38 +0700

    Add Router

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,11 @@ import Beranda from '../pages/Beranda';
 import Profil from '../pages/Profil';
 import Progres from '../pages/Progres';
 import Tersimpan from '../pages/Tersimpan';
+import useAuthStore from '../store/authStore';
+import Login from '../pages/Login';
+import Register from '../pages/Register';
+import Notifikasi from '../pages/Notifikasi';
+import Pengaturan from '../pages/Pengaturan';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -18,26 +23,10 @@ const MainApp = () => {
         unmountOnBlur: true,
       }}
     >
-      <Tab.Screen
-        name="Beranda"
-        component={Beranda}
-        options={{ headerShown: false }}
-      />
-      <Tab.Screen
-        name="Tersimpan"
-        component={Tersimpan}
-        options={{ headerShown: false }}
-      />
-      <Tab.Screen
-        name="Progres"
-        component={Progres}
-        options={{ headerShown: false }}
-      />
-      <Tab.Screen
-        name="Profil"
-        component={Profil}
-        options={{ headerShown: false }}
-      />
+      <Tab.Screen name="Beranda" component={Beranda} options={{ headerShown: false }} />
+      <Tab.Screen name="Tersimpan" component={Tersimpan} options={{ headerShown: false }} />
+      <Tab.Screen name="Progres" component={Progres} options={{ headerShown: false }} />
+      <Tab.Screen name="Profil" component={Profil} options={{ headerShown: false }} />
     </Tab.Navigator>
   );
 };
@@ -60,14 +49,43 @@ const Authenticated = () => {
           animation: 'slide_from_right',
         }}
       />
+      <Stack.Screen
+        name="Notifikasi"
+        component={Notifikasi}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="Pengaturan"
+        component={Pengaturan}
+        options={{
+          headerShown: false,
+        }}
+      />
     </Stack.Navigator>
   );
 };
 
-const UnAuthenticated = () => {};
+const UnAuthenticated = () => {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        gestureEnabled: true,
+        contentStyle: { backgroundColor: '#fafafa' },
+      }}
+      initialRouteName="Login"
+    >
+      <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
+      <Stack.Screen name="Register" component={Register} options={{ headerShown: false }} />
+    </Stack.Navigator>
+  );
+};
 
 const Router = () => {
-  return <Authenticated />;
+  const { isLogin } = useAuthStore();
+
+  return isLogin ? <Authenticated /> : <UnAuthenticated />;
 };
 
 export default Router;
