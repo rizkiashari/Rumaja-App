48994f27e8af48f8ba4f42f0d6d8c7ac191fd6b7
RizkiAshari <rizkiashari1026@gmail.com>
2022-12-25 20:24:14 +0700

    Add Assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -7,8 +7,11 @@ import ProgresActive from './icons/progress_active.svg';
 import ProgresInactive from './icons/progress_inactive.svg';
 import TersimpanActive from './icons/save_active.svg';
 import TersimpanInactive from './icons/save_inactive.svg';
+import ShowPassword from './icons/show_pass.svg';
+import HidePassword from './icons/hide_pass.svg';
 
 // ilustrations
+import ILLogin from './ilustrations/ilustration_login.png';
 
 export {
   BerandaActive,
@@ -19,4 +22,7 @@ export {
   ProgresInactive,
   TersimpanActive,
   TersimpanInactive,
+  ShowPassword,
+  HidePassword,
+  ILLogin,
 };
