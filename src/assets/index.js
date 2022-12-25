b2cbe7e1476a0e31e09aa71d97c72ddef2f18c17
RizkiAshari <rizkiashari1026@gmail.com>
2022-12-25 23:21:40 +0700

    Add Assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -9,9 +9,14 @@ import TersimpanActive from './icons/save_active.svg';
 import TersimpanInactive from './icons/save_inactive.svg';
 import ShowPassword from './icons/show_pass.svg';
 import HidePassword from './icons/hide_pass.svg';
+import SettingBlack from './icons/setting_black.svg';
+import Notification from './icons/notification_black.svg';
+import ChevronBack from './icons/chevron_back.svg';
 
 // ilustrations
 import ILLogin from './ilustrations/ilustration_login.png';
+import ILNotifEmpty from './ilustrations/notif_empty.png';
+import ILPlaceholder from '../assets/ilustrations/placeholder_profile.png';
 
 export {
   BerandaActive,
@@ -24,5 +29,10 @@ export {
   TersimpanInactive,
   ShowPassword,
   HidePassword,
+  SettingBlack,
+  Notification,
+  ChevronBack,
   ILLogin,
+  ILNotifEmpty,
+  ILPlaceholder,
 };
