2a3e120d20c02beef416f0a5711daaee1b9a6545
RizkiAshari <rizkiashari1026@gmail.com>
2022-12-27 14:58:35 +0700

    Update Empty Content

--- src/components/atoms/EmptyContent.js
+++ src/components/atoms/EmptyContent.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import { Box, Image, Text } from 'native-base';
+import { Box, HStack, Image, Text } from 'native-base';
 import { fonts } from '../../utils/fonts';
 import { colors } from '../../utils/colors';
 import { Dimensions } from 'react-native';
+import { InfoBlack } from '../../assets';
 
 const EmptyContent = ({ image, title, subTitle }) => {
   const { width, height } = Dimensions.get('window');
+
+  if (!image) {
+    return (
+      <Box backgroundColor={colors.white} height={height / 6} justifyContent="center" alignItems="center">
+        <HStack space={1} alignItems="center">
+          <InfoBlack />
+          <Text fontFamily={fonts.primary[500]} color="black" fontSize={width / 32}>
+            {title}
+          </Text>
+        </HStack>
+      </Box>
+    );
+  }
 
   return (
     <Box minH={height / 2} justifyContent="center" alignItems="center">
@@ -13,13 +27,7 @@ const EmptyContent = ({ image, title, subTitle }) => {
       <Text fontFamily={fonts.primary[600]} mb="2" fontSize={14}>
         {title}
       </Text>
-      <Text
-        fontFamily={fonts.primary[400]}
-        color={colors.text.black60}
-        fontSize={14}
-        textAlign="center"
-        maxWidth={width / 1.3}
-      >
+      <Text fontFamily={fonts.primary[400]} color={colors.text.black60} fontSize={14} textAlign="center" maxWidth={width / 1.3}>
         {subTitle}
       </Text>
     </Box>
