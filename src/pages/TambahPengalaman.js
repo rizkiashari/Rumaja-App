dbcd2b62e087d9b4c64efbfb3b4141451c92cb99
Rizki Ashari <rizkiashari1026@gmail.com>
2023-01-30 01:12:33 +0700

    remove validate

--- src/pages/TambahPengalaman.js
+++ src/pages/TambahPengalaman.js
@@ -68,12 +68,6 @@ const TambahPengalaman = ({ navigation }) => {
     onSubmit: async (values) => {
       setLoading(true);
 
-      if (values.tahun_mulai > values.tahun_akhir) {
-        showError('Mulai kerja tidak boleh lebih besar dari selesai kerja');
-        setLoading(false);
-        return;
-      }
-
       const payload = {
         nama: values.nama,
         pengalaman_prov: values.pengalaman_prov,
